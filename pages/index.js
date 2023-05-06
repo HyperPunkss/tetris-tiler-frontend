0e5ec2947c8444c77f081a3ad7eac31c070e0abd
EliasSahlos <esahlos12@gmail.com>
2023-05-06 14:59:42 +0300

    Prettier reformation

--- pages/index.js
+++ pages/index.js
@@ -3,68 +3,66 @@ import LetterPicker from "@/components/LetterPicker";
 import SizePicker from "@/components/SizePicker";
 import axios from "axios";
 import Image from "next/image";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import ColRowPicker from "@/components/ColRowPicker";
 
 export default function Home() {
-
-    const [sizeData, setSizeData] = useState('')
-    const [columnData, setColumnData] = useState(5)
-    const [rowData, setRowData] = useState(5)
+    const [sizeData, setSizeData] = useState("");
+    const [columnData, setColumnData] = useState(5);
+    const [rowData, setRowData] = useState(5);
 
     //TASK 2 - Getting all the shape information from the API
-    const [shapes, setShapes] = useState([])
+    const [shapes, setShapes] = useState([]);
 
-  async function task2apiCall() {
-    await axios.get('http://matsaki95.ddns.net:8900/api/v1/a2-task').then((response) => {
-      // setShapes(JSON.parse(response.data))
-      // console.log(response.data);
-      setShapes(response.data)
-    })
-    // setShapes(JSON.parse(testObjJSON))
-    // console.log(shapes);
-    for(let i=0; i<shapes.length; i++){
-      console.log(shapes[i].content);
-    }
-    
-  }
-
-  //TASK 10 - Generating Data and Sending them to the API with Axios
-  function task10apiCall(){
-    var data = JSON.stringify({
-        x: 5,
-        y: 5,
-        holes: [{x: 2, y: 2}],
-        allow_rotations: true,
-        allow_flips: false,
-        shapes: ["T", "L"],
-    });
-
-    // var config = {
-    //   method: 'post',
-    //   url: 'api',
-    //   data: data
-    // }
-    axios.post("/api/user", data).then(
-        (response) => {
-            const data = response.data;
-            //console.log(response)
-        },
-        (error) => {
-          console.log(error);
+    async function task2apiCall() {
+        await axios.get("http://matsaki95.ddns.net:8900/api/v1/a2-task").then((response) => {
+            // setShapes(JSON.parse(response.data))
+            // console.log(response.data);
+            setShapes(response.data);
+        });
+        // setShapes(JSON.parse(testObjJSON))
+        // console.log(shapes);
+        for (let i = 0; i < shapes.length; i++) {
+            console.log(shapes[i].content);
         }
-    );
-  }
+    }
+
+    //TASK 10 - Generating Data and Sending them to the API with Axios
+    function task10apiCall() {
+        var data = JSON.stringify({
+            x: 5,
+            y: 5,
+            holes: [{ x: 2, y: 2 }],
+            allow_rotations: true,
+            allow_flips: false,
+            shapes: ["T", "L"],
+        });
+
+        // var config = {
+        //   method: 'post',
+        //   url: 'api',
+        //   data: data
+        // }
+        axios.post("/api/user", data).then(
+            (response) => {
+                const data = response.data;
+                //console.log(response)
+            },
+            (error) => {
+                console.log(error);
+            }
+        );
+    }
 
     //Callback function
     function handleSizeChange(newSize) {
-        setSizeData(newSize)
+        setSizeData(newSize);
     }
 
     //Callback function for Columns & Rows Size
     function handleColRowChange(newSize) {
-        setColumnData(newSize[0])
-        setRowData(newSize[1])
+        setColumnData(newSize[0]);
+        setRowData(newSize[1]);
     }
 
     return (
@@ -72,14 +70,14 @@ export default function Home() {
             <h1 className="font-bold bg-blue-400 p-2 rounded">TASK 1</h1>
             <div className="flex flex-col">
                 <div className="flex justify-center items-center">
-                    <Grid rows={rowData} columns={columnData} sizeData={sizeData}/>
+                    <Grid rows={rowData} columns={columnData} sizeData={sizeData} />
                 </div>
                 <div className="flex justify-center items-center p-4">
-                    <SizePicker onSizeChange={handleSizeChange}/>
-                    <ColRowPicker onColRowChange={handleColRowChange}/>
+                    <SizePicker onSizeChange={handleSizeChange} />
+                    <ColRowPicker onColRowChange={handleColRowChange} />
                 </div>
                 <div className="ml-32 grid ">
-                    <LetterPicker/>
+                    <LetterPicker />
                 </div>
             </div>
             <h1 className="font-bold bg-blue-400 p-2 rounded">TASK 2</h1>
