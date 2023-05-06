911aeb7b8d73140c0ab3e40eb6d047c6d4b7d5f4
EliasSahlos <esahlos12@gmail.com>
2023-05-06 13:46:14 +0300

    Added functionality on column and row buttons.

--- components/ColRowPicker.jsx
+++ components/ColRowPicker.jsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
-function RowPicker() {
+function ColRowPicker({onColRowChange}) {
+    const [colValue, setColValue] = useState(5)
+    const [rowValue, setRowValue] = useState(5)
+
+    useEffect(() => {
+        onColRowChange([colValue,rowValue])
+    }, [colValue,rowValue])
+
+
     return (
-        <div className="my-4 mx-4">
+        <div className="my-4">
             <div className="flex">
                 <h1 className="text-center font-semibold mr-[2px] mb-2 text-sm ml-[40px]">Columns</h1>
                 <h1 className="text-center font-semibold mr-[2px] mb-2 text-sm ml-[68px]">Rows</h1>
             </div>
+
+            {/*Column*/}
             <input
                 type="number"
                 name=""
@@ -14,8 +24,11 @@ function RowPicker() {
                 className=" border-[2px] border-purple-700 bg-gray-200 p-2 rounded-lg shadow-md w-[80px] mx-8"
                 min={1}
                 max={100}
-                value={5}
+                defaultValue={5}
+                onChange={(e) => setColValue(e.target.value)}
             />
+
+            {/*Row*/}
             <input
                 type="number"
                 name=""
@@ -23,10 +36,11 @@ function RowPicker() {
                 className=" border-[2px] border-purple-700 bg-gray-200 p-2 rounded-lg shadow-md w-[80px]"
                 min={1}
                 max={100}
-                value={5}
+                defaultValue={5}
+                onChange={(e) => setRowValue(e.target.value)}
             />
         </div>
     );
 }
 
-export default RowPicker;
+export default ColRowPicker;
--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,44 @@
 import Grid from "@/components/Grid";
 import LetterPicker from "@/components/LetterPicker";
-import RowPicker from "@/components/RowPicker";
 import SizePicker from "@/components/SizePicker";
 import axios from "axios";
 import Image from "next/image";
-import { useState } from "react";
+import {useState} from "react";
+import ColRowPicker from "@/components/ColRowPicker";
 
 export default function Home() {
-    const [sizeData,setSizeData] = useState('')
-    const [columnData,setColumnData] = useState('')
-    const [rowData,setRowData] = useState('')
+
+    const [sizeData, setSizeData] = useState('')
+    const [columnData, setColumnData] = useState(5)
+    const [rowData, setRowData] = useState(5)
+
     //TASK 2 - Getting all the shape information from the API
-  const [shapes, setShapes] = useState([])
-  
-  const testObj = [
-    {letter: "F", content: "EEEEE EEEEE EEFFE EFFEE EEFEE"},
-    {letter: "I", content: "EEIEE EEIEE EEIEE EEIEE EEIEE"}
-  ]
-  const testObjJSON = JSON.stringify(testObj)
+    const [shapes, setShapes] = useState([])
 
-  function task2apiCall(){
-    // axios.get('/url/user').then((response) => {
-    //   setShapes(JSON.parse(response.data))
-    // })
-    
-    setShapes(JSON.parse(testObjJSON))
-    console.log(shapes);
-    for(let i=0; i<shapes.length; i++){
-      console.log(shapes[i].content);
+    const testObj = [
+        {letter: "F", content: "EEEEE EEEEE EEFFE EFFEE EEFEE"},
+        {letter: "I", content: "EEIEE EEIEE EEIEE EEIEE EEIEE"}
+    ]
+    const testObjJSON = JSON.stringify(testObj)
+
+    function task2apiCall() {
+        // axios.get('/url/user').then((response) => {
+        //   setShapes(JSON.parse(response.data))
+        // })
+
+        setShapes(JSON.parse(testObjJSON))
+        console.log(shapes);
+        for (let i = 0; i < shapes.length; i++) {
+            console.log(shapes[i].content);
+        }
+
     }
-    
-  }
 
-  //TASK 10 - Generating Data and Sending them to the API with Axios 
+    //TASK 10 - Generating Data and Sending them to the API with Axios
     var data = JSON.stringify({
         x: 5,
         y: 5,
-        holes: [{ x: 2, y: 2 }],
+        holes: [{x: 2, y: 2}],
         allow_rotations: true,
         allow_flips: false,
         shapes: ["T", "L"],
@@ -59,18 +61,18 @@ export default function Home() {
 
     //Letter Compare and returning the matching Colour
     const letterArray = [
-        { letter: "F", color: "#001fc4" },
-        { letter: "I", color: "#9c1516" },
-        { letter: "L", color: "#efee29" },
-        { letter: "N", color: "#e719e2" },
-        { letter: "P", color: "#07d4f3" },
-        { letter: "T", color: "#8a8a8a" },
-        { letter: "U", color: "#00e53f" },
-        { letter: "V", color: "#f19a05" },
-        { letter: "W", color: "#d5d5d5" },
-        { letter: "X", color: "#2097b8" },
-        { letter: "Y", color: "#b82082" },
-        { letter: "Z", color: "#ff0011" },
+        {letter: "F", color: "#001fc4"},
+        {letter: "I", color: "#9c1516"},
+        {letter: "L", color: "#efee29"},
+        {letter: "N", color: "#e719e2"},
+        {letter: "P", color: "#07d4f3"},
+        {letter: "T", color: "#8a8a8a"},
+        {letter: "U", color: "#00e53f"},
+        {letter: "V", color: "#f19a05"},
+        {letter: "W", color: "#d5d5d5"},
+        {letter: "X", color: "#2097b8"},
+        {letter: "Y", color: "#b82082"},
+        {letter: "Z", color: "#ff0011"},
         {letter: "B", color: "#000000"},
         {letter: "E", color: "#ffffff"}
     ];
@@ -85,31 +87,37 @@ export default function Home() {
     //console.log(result[0]);
 
     //Callback function
-    function HandleSizeChange(newSize){
+    function handleSizeChange(newSize) {
         setSizeData(newSize)
     }
 
-    console.log(sizeData);
+    function handleColRowChange(newSize) {
+        setColumnData(newSize[0])
+        setRowData(newSize[1])
+    }
+
+    console.log('COL DATA', columnData)
+    console.log('ROW DATA', rowData)
     return (
         <div className="my-4 p-2">
             <h1 className="font-bold bg-blue-400 p-2 rounded">TASK 1</h1>
             <div className="flex flex-col">
                 <div className="flex justify-center items-center">
-                    <Grid rows={5} columns={5} sizeData={sizeData} />
+                    <Grid rows={rowData} columns={columnData} sizeData={sizeData}/>
                 </div>
                 <div className="flex justify-center items-center p-4">
-                    <SizePicker onSizeChange={HandleSizeChange}/>
-                    <RowPicker />
+                    <SizePicker onSizeChange={handleSizeChange}/>
+                    <ColRowPicker onColRowChange={handleColRowChange}/>
                 </div>
                 <div className="ml-32 grid ">
-                    <LetterPicker />
+                    <LetterPicker/>
                 </div>
             </div>
             <h1 className="font-bold bg-blue-400 p-2 rounded">TASK 2</h1>
             <div className="">
-              <button className="rounded bg-white p-2 m-4 border-2 border-black flex" onClick={task2apiCall}>
-                Generate all the shapes!
-              </button>
+                <button className="rounded bg-white p-2 m-4 border-2 border-black flex" onClick={task2apiCall}>
+                    Generate all the shapes!
+                </button>
             </div>
         </div>
     );
