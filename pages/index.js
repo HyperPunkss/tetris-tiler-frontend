22dca32691552cfcbf67194a6980e521e4e65851
AlexaderLoco <alexlocopap@gmail.com>
2023-05-06 10:17:58 +0300

    added a way to get the colour from the corresponding letter

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 import Grid from '@/components/Grid'
 import axios from 'axios'
+import { Input } from 'postcss'
 
 export default function Home() {
+  
+  //Start of data and Axios call to the API
   var data = JSON.stringify({
     x: 5,
     y: 5,
@@ -16,13 +19,37 @@ export default function Home() {
   //   url: 'api',
   //   data: data
   // }
-  
   axios.post('/api/user', data).then((response)=>{
     const data = response.data
-    console.log(response)
+    //console.log(response)
   }, (error)=>{
-    console.log(error);
+    //console.log(error);
   })
+
+
+  //Letter Compare and returning the matching Colour 
+  const letterArray = [
+    {letter: "F", color: "#001fc4"},
+    {letter: "I", color: "#9c1516"},
+    {letter: "L", color: "#efee29"},
+    {letter: "N", color: "#e719e2"},
+    {letter: "P", color: "#07d4f3"},
+    {letter: "T", color: "#8a8a8a"},
+    {letter: "U", color: "#00e53f"},
+    {letter: "V", color: "#f19a05"},
+    {letter: "W", color: "#d5d5d5"},
+    {letter: "X", color: "#2097b8"},
+    {letter: "Y", color: "#b82082"},
+    {letter: "Z", color: "#ff0011"}
+  ]
+  var input
+  const result = letterArray.filter((item) => {
+    return item.letter === input
+  }).map((item) => {
+    return item.color
+  })
+  //console.log(result[0]);
+ 
   
   return (
     <div>
