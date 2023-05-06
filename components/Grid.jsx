9b54790bd1fb27367e845bf6a241e98a467b76bf
EliasSahlos <esahlos12@gmail.com>
2023-05-06 09:41:57 +0300

    User can pick a size option

--- components/Grid.jsx
+++ components/Grid.jsx
@@ -1,24 +1,47 @@
-import React from "react";
+import React, { useState } from 'react';
 
 const Grid = ({ rows, columns }) => {
-    const handleClick = (id) => {
-        console.log(id);
-    };
+  const [cellSize, setCellSize] = useState('medium');
 
-    const grid = [];
-    for (let i = 0; i < rows; i++) {
-        const row = [];
-        for (let j = 0; j < columns; j++) {
-            const id = `${i}-${j}`;
-            row.push(<div className="w-5 h-5 border border-black" key={id} onClick={() => handleClick(id)}></div>);
-        }
-        grid.push(
-            <div className="flex" key={i}>
-                {row}
-            </div>
-        );
+  const handleClick = (id) => {
+    console.log(id);
+  };
+
+  const handleSizeChange = (event) => {
+    setCellSize(event.target.value);
+  };
+
+  const cellClasses = {
+    small: 'w-2 h-2',
+    medium: 'w-8 h-8',
+    large: 'w-10 h-10',
+  };
+
+  const grid = [];
+  for (let i = 0; i < rows; i++) {
+    const row = [];
+    for (let j = 0; j < columns; j++) {
+      const id = `${i}-${j}`;
+      row.push(
+        <div
+          className={`${cellClasses[cellSize]} border border-black`}
+          key={id}
+          onClick={() => handleClick(id)}
+        ></div>
+      );
     }
-    return <div className="flex flex-col">{grid}</div>;
+    grid.push(<div className="flex" key={i}>{row}</div>);
+  }
+  return (
+    <div>
+      <select onChange={handleSizeChange}>
+        <option value="small">Small</option>
+        <option value="medium">Medium</option>
+        <option value="large">Large</option>
+      </select>
+      <div className="flex flex-col">{grid}</div>
+    </div>
+  );
 };
 
 export default Grid;
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,9 @@ import Grid from '@/components/Grid'
 export default function Home() {
   return (
     <div>
+        <div className='p-2 flex justify-center items-center'>
          <Grid rows={3} columns={21} />
+        </div>
     </div>
   )
 }
